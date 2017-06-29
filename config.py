484dc464919b751fb1fdb3470b6c03d06fc96d69
范薇薇 <fanweiweiniuyue@gmail.com>
2017-06-29 01:28:13 -0400

    Improved directory spinup

--- config.py
+++ config.py
@@ -10,13 +10,17 @@ run_dir = os.getcwd()
 
 #######################################################
 
+images_path = './images'
+
+original_jpg_image_path = images_path + '/original-jpg'
+
 small_train_image_path = './images/small/train'
 small_test_image_path  = './images/small/test'
 
 medium_train_image_path = './images/medium/train'
 medium_test_image_path  = './images/medium/test'
 
-original_train_image_path = './images/original/train'
+original_jpg_train_image_path = original_jpg_image_path + '/train'
 
 original_test_image_path_one  = './images/original/test'
 original_test_image_path_two  = './images/original/test-jpg-additional'
--- spinup.py
+++ spinup.py
@@ -8,35 +8,6 @@
 
 from clint.textui import colored, puts, indent
 
-
-'''
-    if arguments['validate']:
-        with indent(4):
-            puts(colored.magenta('\nAll good\n'))
-            puts('hello world\n\n')
-
-
-            img_resize = (16, 16)
-        color_channels = 3  # RGB
-        train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file = data_helper.get_jpeg_data_files_paths()
-
-        assert os.path.exists(train_jpeg_dir), "The {} folder does not exist".format(train_jpeg_dir)
-        assert os.path.exists(test_jpeg_dir), "The {} folder does not exist".format(test_jpeg_dir)
-        assert os.path.exists(train_csv_file), "The {} file does not exist".format(test_jpeg_additional)
-        assert os.path.exists(train_csv_file), "The {} file does not exist".format(train_csv_file)
-
-        x_train, x_test, y_train, y_map, x_test_filename = data_helper.preprocess_data(train_jpeg_dir, test_jpeg_dir,
-                                                                                       test_jpeg_additional,
-                                                                                       train_csv_file, img_resize)
-        labels_df = pd.read_csv(train_csv_file)
-        labels_count = len(set(chain.from_iterable([tags.split(" ") for tags in labels_df['tags'].values])))
-        train_files_count = len(os.listdir(train_jpeg_dir))
-        test_files_count = len(os.listdir(test_jpeg_dir)) + len(os.listdir(test_jpeg_additional))
-        assert x_train.shape == (train_files_count, *img_resize, color_channels)
-        assert x_test.shape == (test_files_count, *img_resize, color_channels)
-        assert y_train.shape == (train_files_count, labels_count)
-'''
-
 import os
 import config
 import credentials
@@ -48,26 +19,45 @@ def command(command, path, log=True):
     else:
         subprocess.call([command], shell=True, cwd=path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
+directory_structure = [
+    config.tmp_dir,
+    config.images_path,
+    config.original_jpg_image_path,
+    config.original_jpg_train_image_path
+]
+
+def validate_directory_structure(directories):
+    try:
+        head, *tail = directories
+
+        if not head:
+            puts(colored.green('directories ok'))
+            return
+
+        if not os.path.isdir(head):
+            puts(colored.red('creating {}'.format(head)))
+            command('mkdir {}'.format(head), config.run_dir)
+
+        validate_directory_structure(tail)
+
+    except ValueError: pass
+
+
+def validate_original_jpg_train_images(tries):
+    if tries == 0:
+        puts(colored.green('failed to validate training images'))
+        return False
+
+
+    puts(colored.green('full size jpgs ok'))
+    return True
+
 
 def validate_train_csv(tries):
 
     if tries == 0:
         puts(colored.green('failed to create training csv'))
         return False
-
-    ## CHECK IF TMP EXISTS
-    tmp_exists = os.path.isdir(config.tmp_dir)
-    if not tmp_exists:
-        puts(colored.red('creating tmp directory'))
-        create_directory(config.tmp_dir, config.run_dir)
-        validate_train_csv(tries - 1)
-
-    ## CHECK IF DIRECTORY EXISTS
-    directory_exists = os.path.isdir(config.training_data_path)
-    if not directory_exists:
-        puts(colored.red('creating train csv directory'))
-        create_directory(config.training_data_path, config.run_dir)
-        validate_train_csv(tries - 1)
 
     ## CHECK IF FILE EXISTS
     file_exists = os.path.isfile(config.train_csv_path)
@@ -78,12 +68,10 @@ def validate_train_csv(tries):
         unzip_tar('train_v2.csv.zip', config.tmp_dir)
         move_from_tmp('train_v2.csv', config.tmp_dir, config.train_csv_path)
         clean_tmp()
-        validate_train_csv(tries - 1)
+        return validate_train_csv(tries - 1)
 
-    valid = tmp_exists and directory_exists and file_exists
-    if valid:
-        puts(colored.green('training csv ok'))
-        return True
+    puts(colored.green('training csv ok'))
+    return True
 
 
 def download_kaggle_file(file, path):
@@ -110,10 +98,6 @@ def remove_macosx(file, path):
 def move_from_tmp(file, path, new):
     command('mv {} ../{}'.format(file, new), path, log=False)
 
-def create_directory(name, path):
-    command('mkdir {}'.format(name), path)
-
-
 def clean_tmp():
     command('rm -r tmp/*', config.run_dir)
 
@@ -121,8 +105,10 @@ def launch():
 
     with indent(4):
 
-        puts(colored.magenta('\nInitialising\n'))
+        puts(colored.magenta('\ninitialising\n'))
+        validate_directory_structure(directory_structure)
         validate_train_csv(5)
+        validate_original_jpg_train_images(5)
 
 
 
